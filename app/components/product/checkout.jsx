90bc3c103ce27a76d9fc45e50a5c9936c26681ab
nguyenvinhon <nhon@carbon8.com>
2017-07-15 19:56:04 +0700

    push

--- app/components/product/checkout.jsx
+++ app/components/product/checkout.jsx
@@ -15,54 +15,51 @@ return(
 		<div className="container">
 			<h3>THANH TOÁN</h3>
 			<div className="row  row-40">
-				<div className="col-sm-8">
+				<div className="col-sm-7">
 					<div className="info-custom">
 					<div className="row">
-						<div className="col-sm-4">
+						<div className="col-sm-3">
 							<p>Email</p>
 						</div>
-						<div className="col-sm-8">
+						<div className="col-sm-9">
 							<div className="from-group">
 								<input type="text" className="form-control" />
 							</div>
 						</div>
 					</div>
 					<div className="row">
-						<div className="col-sm-4">
+						<div className="col-sm-3">
 							<p>Họ và tên</p>
 						</div>
-						<div className="col-sm-8">
+						<div className="col-sm-9">
 							<div className="from-group">
 								<input type="text" className="form-control" />
 							</div>
 						</div>
 					</div>
 					<div className="row">
-						<div className="col-sm-4">
+						<div className="col-sm-3">
 							<p>Địa chỉ</p>
 						</div>
-						<div className="col-sm-8">
+						<div className="col-sm-9">
 							<div className="from-group">
 								<input type="text" className="form-control" />
 							</div>
 						</div>
 					</div>
 					<div className="row">
-						<div className="col-sm-4">
+						<div className="col-sm-3">
 							<p>Số điện thoại</p>
 						</div>
-						<div className="col-sm-8">
+						<div className="col-sm-9">
 							<div className="from-group">
 								<input type="text" className="form-control" />
 							</div>
 						</div>
 					</div>
-					<p className="text-center">
-					<a className="btn-more btn-red" href="#">xác thực</a>
-					</p>
 				</div>
 				</div>
-				<div className="col-sm-4">
+				<div className="col-sm-5">
 					<div className="right-cart">
 						<h4>Thông tin đơn hàng</h4>
 						<table>
@@ -94,12 +91,13 @@ return(
 						<hr />
 						<p>Thành tiền <span>(Tổng số tiền thanh toán)</span></p>
 						<p className="text-right rate">1.3600.000đ</p>
-						<p className="text-center">
-							<a className="btn-more btn-red" href="#">Thanh toán</a>
-						</p>
+						
 					</div>
 				</div>
 			</div>
+			<p className="text-center">
+							<a className="btn-more btn-red" href="#">Xác nhận Thanh toán</a>
+						</p>
 		</div>
 	</div>
 	<Footer />
